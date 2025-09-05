406129bd2ae978342872b0b90e85565fe57ee066
naerickson334 <naerickson@augusta.edu>
2025-09-05 19:00:07 -0400

    Update parity.py 1

--- 01-parity/parity.py
+++ 01-parity/parity.py
@@ -1,9 +1,12 @@
 # Prompt the user to enter an integer number
-
+x = int(input("Enter an integer number: "))
 
 
 # Determine if the number is even or odd
 #   If it is even print (e.g.) "4 is even."
 #   If it is odd, print (e.g.) "7 is odd."
 
-
+if x % 2 == 0:
+    print(f"{x} is even.")
+else:
+    print(f"{x} is odd.")
