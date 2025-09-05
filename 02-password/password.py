ea826208b91fdd1f92a40a93984bd6c5fbea1986
naerickson334 <naerickson@augusta.edu>
2025-09-05 19:01:02 -0400

    Update password.py 2

--- 02-password/password.py
+++ 02-password/password.py
@@ -1,9 +1,11 @@
 # Prompt the user to enter a password
-
+x = input("Please enter a password: ")
 
 # Prompt the user to confirm the password
-
+y = input("Confirm password: ")
 
 # Check if the password and confirmation match
-
-
+if x == y:
+    print("Passwords match.")
+else:
+    print("Passwords do not match.")
